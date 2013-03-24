f9d24e810ebf4a538fa9fcaecd3efa99a0b95ac7
frmaria <mariabrilaki@gmail.com>
2013-03-24 11:27:52 -0700

    Add user microposts, end of chapter 10

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,8 +63,23 @@ describe "Authentication" do
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
+          end
         end
       end
+
 
       describe "in the Users controller" do
 
@@ -76,7 +91,7 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
            specify { response.should redirect_to(signin_path) }
-        end
+        
       end
 
       describe "visiting the user index" do
@@ -84,6 +99,22 @@ describe "Authentication" do
         it { should have_selector('title', text: 'Sign in') }
       end
     end
+    end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    
+  
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,23 +9,18 @@ describe "index" do
   let(:user) { FactoryGirl.create(:user) }
 
   before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all)  { User.delete_all }
-
+  after(:all)  { User.delete_all }
 
     before(:each) do
       sign_in user
       visit users_path
     end
-
     
     it { should have_selector('title', text: 'All users') }
     it { should have_selector('h1',    text: 'All users') }
 
-    describe "pagination" do
-
-      
+    describe "pagination" do      
       it { should have_selector('div.pagination') }
-
 
      it "should list each user" do
        User.paginate(page: 1).each do |user|
@@ -33,8 +28,6 @@ describe "index" do
         end
        end
      end
-
-
   
   describe "delete links" do
 
@@ -54,7 +47,6 @@ describe "index" do
         it { should_not have_link('delete', href: user_path(admin)) }
       end
   end
-
 end
 
 
@@ -62,10 +54,19 @@ end
   describe "profile page" do
 	let(:user) { FactoryGirl.create(:user) }
 	  # Code to make a user variable
-	  before { visit user_path(user) }
+  let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+  let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+	  
+    before { visit user_path(user) }
 
 	  it { should have_selector('h1',    text: user.name) }
 	  it { should have_selector('title', text: user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
 	end
 
   describe "signup page" do
